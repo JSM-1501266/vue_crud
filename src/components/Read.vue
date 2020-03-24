c30de43314b8b9d27370bfff6db6437d5b33113b
jsm <gkswk89@naver.com>
2020-03-24 10:25:16 +0900

    create vue board

---
+++ src/components/Read.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1>게시판</h1>
+        <table border="2" style="margin: auto">
+            <tr>
+                <td>글쓴이</td>
+                <td>제목</td>
+                <td>내용</td>
+            </tr>
+            <tr :key="index" v-for="(value, index) in data" @click="detail(index)">
+                <td>{{value.writer}}</td>
+                <td>{{value.title}}</td>
+                <td>{{value.content}}</td>
+            </tr>
+        </table> 
+        <br />
+        <button @click="write">글쓰기</button>
+    </div>
+</template>
+
+<script>
+import data from '@/data'
+
+export default {
+    name: 'Read',
+    data() {
+        return {
+            data: data
+        }
+    },
+    methods: {
+        write() {
+            this.$router.push({
+                name: 'Create'
+            })
+        },
+        detail(index) {
+            this.$router.push({
+                name: 'Detail',
+                params: {
+                    contentId: index
+                }
+            })
+        }
+    }
+}
+</script> 
---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        writer: '정성민',
+        title: '아이즈원',
+        content: '1'
+    }, 
+    {
+        writer: '김명준',
+        title: '소녀시대',
+        content: '2'
+    }, 
+    {
+        writer: '민윤기',
+        title: '레드벨벳',
+        content: '3'
+    }
+]
